d04e608477dc77cb1a21930f641d8f4f48bd1c91
Edward2701 <edenismoreld27@gmail.com>
2023-04-12 13:15:11 +0200

    FOTO3

---
+++ repositorio.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+clear
+# git init
+git config user.name "Edward2701"
+git config user.email "edenismoreld27@gmail.com"
+git config user.password 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
+# git add .
+# git commit -m "SNAPSHOT 1"
+git remote add origin "https://github.com/Edward2701/REPOSITORIO-12-04-2023.git"
+git branch -M main
+git push -u origin main 
